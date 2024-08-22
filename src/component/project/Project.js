4324c2eca1e18c02b24d1d41aab1eae9b0e90b98
ehsan <e.alavi1380@gmail.com>
2024-08-22 11:13:47 -0800

    edit

--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -17,6 +17,10 @@ const Project = () => {
             <h3  data-aos="fade-right" className='ml-3 mt-5'>Hint : to see any project click on title</h3>
             <div className='w-full h-[80%] br2:h-[50%] flex flex-col-reverse br2:flex-row flex-wrap justify-around px-5 br2:mt-8'>
                 <div data-aos="fade-right" className='br2:w-1/2'>
+                    <a className='flex items-center' href='https://github.com/Alavi11/Social_Media'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>Social Media</p></a>
+                    <p className='ml-6 max-w-xs'>React, Axios , context are is used in the front part of the project and Node , Express , Mongoose and JWT and etc are used in the back part of the project and the structure of Access-Token and Refresh-Token is implemented .<br/>Routing and login sections and registration and restriction of routes have been done in this project</p>
+                </div>
+                <div data-aos="fade-right" className='br2:w-1/2'>
                     <a className='flex items-center' href='https://github.com/Alavi11/Amlak'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>Estate</p></a>
                     <p className='ml-6 max-w-xs'>React, TypeScript, Tailwind, Axios are is used in the front part of the project and Node and Express are used in the back part of the project.<br/>Routing and login sections and registration and restriction of routes have been done in this project</p>
                 </div>
