b8bf5dc49944c9e41225a9d86c1224f8a0d4b745
ehsan <e.alavi1380@gmail.com>
2024-10-06 21:49:46 -0800

    edit

--- src/component/about/About.js
+++ src/component/about/About.js
@@ -14,7 +14,7 @@ const About = () => {
     })
 
 
-  return <div className='w-full h-[2700px] br2:h-[1900px] br6:h-[2000px] br4:px-20 pt-5 br4:pt-24 overflow-hidden' id='about'>
+  return <div className='w-full h-[2700px] br5:h-[2300px] br2:h-[1900px] br6:h-[2000px] br4:px-20 pt-5 br4:pt-24 overflow-hidden' id='about'>
             <h1 className='text-blue-950 text-3xl  ml-3 w-12 h-14 border-b-[3px] border-b-[#149ddd]'>About</h1>
             <p data-aos="fade-left" className='mt-7 mb-10 ml-3 w-[95%]'>
             Hello <br></br>
@@ -45,9 +45,9 @@ const About = () => {
                     </ul>
                 </div>
             </div>
-            <div className='w-full br5:bg-white br7:mt-24 h-[800px] br2:h-[500px] br4:h-[500px] bg-blue-50'>
+            <div className='w-full br5:bg-white br7:mt-24 h-[1000px] br2:h-[700px] br4:h-[500px]'>
                 <h1 className='text-blue-950 text-3xl mt-8 ml-3 w-12 h-14 border-b-[3px] border-b-[#149ddd]'>Skills</h1>
-                <div className='mt-5 w-full br2:flex-row br2:flex-wrap h-[83%] br4:h-[70%] px-[2.5%] flex flex-col-reverse justify-between'>
+                <div className='mt-5 w-full br2:flex-row br2:flex-wrap h-[90%] br4:h-[70%] px-[2.5%] flex flex-col-reverse justify-between'>
                         {
                             skills.map(item => {
                                 return <Progressbar percent = {item.percent} name={item.name}/>
--- src/component/project/Project.js
+++ src/component/project/Project.js
@@ -12,22 +12,21 @@ const Project = () => {
     })
 
 
-  return <div className='w-full h-[800px] br2:h-[700px] br4:pt-24 mt-1 overflow-x-hidden br4:px-0' id='projects'>
+  return <div className='w-full h-[600px] br2:h-[700px] br4:pt-24 mt-1  br4:px-0' id='projects'>
             <h1  data-aos="fade-left" className='text-blue-950 text-3xl  ml-3 w-12 h-14 border-b-[3px] border-b-[#149ddd]'>Projects</h1>
             <h3  data-aos="fade-right" className='ml-3 mt-5'>Hint : to see any project click on title</h3>
-            <div className='w-full h-[80%] br2:h-[50%] flex flex-col-reverse br2:flex-row flex-wrap justify-around px-5 br2:mt-8 overflow-y-scroll'>
-                <div data-aos="fade-right" className='br2:w-1/2'>
-                    <a className='flex items-center' href='https://github.com/Alavi11/netflix'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>NetFlix</p></a>
+            <div className='w-full flex flex-wrap gap-8  px-5 overflow-y-scroll mt-5'>
+                <div data-aos="fade-right" >
+                    <a className='flex items-center' href='https://github.com/Alavi11/netflix'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>NETFLIX</p></a>
                     <p className='ml-6 max-w-xs'>React, Tailwind, Axios , Node ,Express ,JWT</p>
                 </div>
-                <div data-aos="fade-right" className='br2:w-1/2'>
+                <div data-aos="fade-right">
+                    <a className='flex items-center' href='https://admin-panel-e.liara.run/'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>Admin DashBoard</p></a>
+                    <p className='ml-6 max-w-xs'>React and Tailwind and ReChart</p>
+                </div>
+                <div data-aos="fade-right">
                     <a className='flex items-center' href='https://github.com/Alavi11/Social_Media'><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>Social Media</p></a>
                     <p className='ml-6 max-w-xs'>React, Axios , context are is used in the front part of the project and Node , Express , Mongoose and JWT and etc are used in the back part of the project and the structure of Access-Token and Refresh-Token is implemented.</p>
-                </div>
-                
-                <div data-aos="fade-right" className='br2:w-1/2 br2:h-20'>
-                    <a className='flex items-center' href=''><BsCircleFill className='text-[10px] mr-1 text-[#149ddd]'/><p className='text-2xl'>Admin DashBoard</p></a>
-                    <p className='ml-6 max-w-xs'>React and Tailwind and ReChart</p>
                 </div>
             </div>
   </div>
